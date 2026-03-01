c26faeeae52bb7081b3662a6294dd2a77038db7c
mothy-08 <119960367+mothy-08@users.noreply.github.com>
2026-03-01 20:48:23 +0800

    ready for deployment

--- main.js
+++ main.js
@@ -1,36 +1,31 @@
-const emailInput = document.getElementById("email");
-const subscribeButton = document.getElementById("subscribe-button");
+const signupCard = document.getElementById("sign-up-card");
 const form = document.getElementById("form");
 const errorMessage = document.getElementById("error-message");
-const signupCard = document.getElementById("sign-up-card");
 const successCard = document.getElementById("success-card");
 const successCardMessage = document.getElementById("success-card-message");
-
-emailInput.addEventListener("input", (e) => {
-  const isEmpty = !e.target.value.trim();
-
-  e.target.dataset.empty = String(isEmpty);
-  errorMessage.dataset.invalid = String(false);
-
-  const isValid = !isEmpty && form.reportValidity();
-  subscribeButton.disabled = !isValid;
-
-  if (!isEmpty && !isValid) {
-    errorMessage.dataset.invalid = String(true);
-  }
-});
+const dismissButton = successCard.querySelector("button");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const email = Object.fromEntries(new FormData(e.target)).email;
 
-  signupCard.dataset.active = String(false);
-  successCard.dataset.active = String(true);
+  if (!form.reportValidity()) {
+    errorMessage.dataset.invalid = "true";
+    return;
+  }
 
+  signupCard.hidden = true;
+  successCard.hidden = false;
   successCardMessage.innerHTML = `
-          A confirmation email has been sent to <b>${email}</b>.
-          Please open it and click the button inside to confirm your
-          subscription.
-        `;
+    A confirmation email has been sent to <b>${email}</b>.
+    Please open it and click the button inside to confirm your subscription.
+  `;
+});
+
+dismissButton.addEventListener("click", () => {
+  successCard.hidden = true;
+  signupCard.hidden = false;
+  form.reset();
+  errorMessage.dataset.invalid = "false";
 });
